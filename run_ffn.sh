3d5d8e919a52772cab2130af40cbedce7a2be953
jiangkuaixue123 <jiangxiaozhou111@163.com>
2026-03-05 14:53:51 +0800

    add
    Signed-off-by: jiangkuaixue123 <jiangxiaozhou111@163.com>

--- run_ffn.sh
+++ run_ffn.sh
@@ -17,41 +17,44 @@ export NCCL_GRAPH_MIXING_SUPPORT=1
 export NCCL_NET_PLUGIN=none
 export NCCL_IB_DISABLE=1
 
-vllm serve "/home/fq9hpsac/fq9hpsacuser03/deepseek-v2-lite" \
-    -dp=2 \
-    --enable_expert_parallel \
-    --compilation-config '{
-		"cudagraph_mode": "FULL_DECODE_ONLY",
-		"cudagraph_capture_sizes": [256]
-	}' \
-    --enable-dbo \
-    --dbo-prefill-token-threshold 12 \
-    --dbo-decode-token-threshold 2 \
-    --port 8021 \
-    --afd-config '{
-        "afd_connector":"p2pconnector",
-        "num_afd_stages":"2",
-        "afd_role": "ffn",
-        "afd_host":"10.248.12.142",
-        "afd_port":"29531",
-        "afd_extra_config":{
-            "afd_size":"2A2F"
-        }
-    }' > ffn.log 2>&1 &
-
-
 # vllm serve "/home/fq9hpsac/fq9hpsacuser03/deepseek-v2-lite" \
 #     -dp=2 \
-#     --port 8021 \
-#     --enforce_eager \
 #     --enable_expert_parallel \
+#     --compilation-config '{
+# 		"cudagraph_mode": "FULL_DECODE_ONLY",
+# 		"cudagraph_capture_sizes": [256]
+# 	}' \
+#     --enable-dbo \
+#     --dbo-prefill-token-threshold 12 \
+#     --dbo-decode-token-threshold 2 \
+#     --port 8021 \
 #     --afd-config '{
 #         "afd_connector":"p2pconnector",
-#         "num_afd_stages":"1",
+#         "num_afd_stages":"2",
 #         "afd_role": "ffn",
-#         "afd_host":"127.0.0.1",
-#         "afd_port":"29521",
+#         "afd_host":"10.248.12.142",
+#         "afd_port":"29531",
 #         "afd_extra_config":{
 #             "afd_size":"2A2F"
 #         }
 #     }' > ffn.log 2>&1 &
+
+
+vllm serve "/home/fq9hpsac/fq9hpsacuser03/deepseek-v2-lite" \
+    -dp=2 \
+    --port 8021 \
+    --enforce_eager \
+    --enable_expert_parallel \
+    --enable-dbo \
+    --dbo-prefill-token-threshold 12 \
+    --dbo-decode-token-threshold 2 \
+    --afd-config '{
+        "afd_connector":"p2pconnector",
+        "num_afd_stages":"2",
+        "afd_role": "ffn",
+        "afd_host":"127.0.0.1",
+        "afd_port":"29521",
+        "afd_extra_config":{
+            "afd_size":"2A2F"
+        }
+    }' > ffn.log 2>&1 &
