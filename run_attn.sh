024261be454b48c6f95a985ced0ae16fb57ee7b1
jiangkuaixue123 <jiangxiaozhou111@163.com>
2026-03-05 14:37:05 +0800

    add
    Signed-off-by: jiangkuaixue123 <jiangxiaozhou111@163.com>

--- run_attn.sh
+++ run_attn.sh
@@ -18,57 +18,54 @@ rm -rf ~/.cache/vllm/
 # -cc.cudagraph_mode=NONE \
 # -cc.mode=0 \
 
-# export NCCL_MAX_NCHANNEL=64
-# export NCCL_BUFFSIZE=16777216
 export BATCH_SIZE=${2:-64}
 # export VLLM_NCCL_SO_PATH=/home/fq9hpsac/fq9hpsacuser03/sources/nccl/build/lib/libnccl.so.2.29.3
 export NCCL_GRAPH_MIXING_SUPPORT=1
 # export NCCL_P2P_NET_CHUNKSIZE=262144
 # export NCCL_P2P_NET_CHUNKSIZE=524288
-# export CUDA_LAUNCH_BLOCKING=1
 export NCCL_NET_PLUGIN=none
 export NCCL_IB_DISABLE=1
 
-# vllm serve "/home/fq9hpsac/fq9hpsacuser03/deepseek-v2-lite" \
-#     --max-num-batched-tokens $BATCH_SIZE \
-#     --data-parallel-size=4 \
-#     --enable_expert_parallel \
-#     --enable-dbo \
-#     --dbo-prefill-token-threshold 12 \
-#     --dbo-decode-token-threshold 2 \
-#     --port 8022 \
-#     --no-enable-prefix-caching \
-#     --compilation-config '{
-# 		"cudagraph_mode": "FULL_DECODE_ONLY",
-# 		"cudagraph_capture_sizes": ['$BATCH_SIZE']
-# 	}' \
-#     --kv-transfer-config '{
-#         "kv_connector": "MooncakeConnector",
-#         "kv_role": "kv_consumer"
-#     }' \
-#     --afd-config '{
-#         "afd_connector":"p2pconnector",
-#         "afd_role": "attention",
-#         "afd_host":"10.248.12.142",
-#         "afd_port":"29531",
-#         "num_afd_stages":"2",
-#         "afd_extra_config":{
-#             "afd_size":"4A2F"
-#         }
-#     }' > attn.log 2>&1 &
-
 vllm serve "/home/fq9hpsac/fq9hpsacuser03/deepseek-v2-lite" \
+    --max-num-batched-tokens $BATCH_SIZE \
     --data-parallel-size=2 \
-    --enforce-eager \
     --enable_expert_parallel \
+    --enable-dbo \
+    --dbo-prefill-token-threshold 12 \
+    --dbo-decode-token-threshold 2 \
     --port 8022 \
+    --no-enable-prefix-caching \
+    --compilation-config '{
+		"cudagraph_mode": "FULL_DECODE_ONLY",
+		"cudagraph_capture_sizes": ['$BATCH_SIZE']
+	}' \
+    --kv-transfer-config '{
+        "kv_connector": "MooncakeConnector",
+        "kv_role": "kv_consumer"
+    }' \
     --afd-config '{
         "afd_connector":"p2pconnector",
         "afd_role": "attention",
-        "afd_host":"127.0.0.1",
-        "afd_port":"29521",
-        "num_afd_stages":"1",
+        "afd_host":"10.248.12.142",
+        "afd_port":"29531",
+        "num_afd_stages":"2",
         "afd_extra_config":{
             "afd_size":"2A2F"
         }
     }' > attn.log 2>&1 &
+
+# vllm serve "/home/fq9hpsac/fq9hpsacuser03/deepseek-v2-lite" \
+#     --data-parallel-size=2 \
+#     --enforce-eager \
+#     --enable_expert_parallel \
+#     --port 8022 \
+#     --afd-config '{
+#         "afd_connector":"p2pconnector",
+#         "afd_role": "attention",
+#         "afd_host":"127.0.0.1",
+#         "afd_port":"29521",
+#         "num_afd_stages":"1",
+#         "afd_extra_config":{
+#             "afd_size":"2A2F"
+#         }
+#     }' > attn.log 2>&1 &
