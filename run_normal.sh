0c43d789dde21ca8bca0f48c80b86e44739fb17e
jiangkuaixue123 <jiangxiaozhou111@163.com>
2026-03-05 16:10:41 +0800

    add
    Signed-off-by: jiangkuaixue123 <jiangxiaozhou111@163.com>

--- run_normal.sh
+++ run_normal.sh
@@ -1,32 +1,35 @@
 #!/bin/bash
-    # --enforce-eager \
 export CUDA_VISIBLE_DEVICES=$1
 export VLLM_TORCH_PROFILER_DIR=./vllm_profile
 export VLLM_TORCH_PROFILER_WITH_STACK=0
 
+export BATCH_SIZE=${2:-64}
+rm -rf /tmp/torchinductor_root/
+
+vllm serve --model="/home/fq9hpsac/fq9hpsacuser03/deepseek-v2-lite" \
+    --data-parallel-size 8 \
+    -tp 1 \
+    --profiler-config.profiler cuda \
+    --enable-expert-parallel \
+	--max-num-batched-tokens $BATCH_SIZE \
+    --no-enable-prefix-caching \
+    --compilation-config '{
+		"cudagraph_mode": "FULL_DECODE_ONLY",
+		"cudagraph_capture_sizes": ['$BATCH_SIZE']
+	}' \
+    --kv-transfer-config '{
+        "kv_connector": "MooncakeConnector",
+        "kv_role": "kv_consumer"
+    }' \
+    > normal.log 2>&1 &
+
 
 # vllm serve --model="/home/fq9hpsac/fq9hpsacuser03/deepseek-v2-lite" \
 #     --data-parallel-size 2 \
+#     --enforce-eager \
 #     -tp 1 \
-#     --enable-expert-parallel \
-# 	--max-num-batched-tokens 72 \
-#     --compilation-config '{
-# 		"cudagraph_mode": "FULL_DECODE_ONLY",
-# 		"cudagraph_capture_sizes": [72]
-# 	}' \
-#     > normal.log 2>&1 &
-rm -rf /tmp/torchinductor_root/
-
-# --kv-transfer-config '{
-#         "kv_connector": "MooncakeConnector",
-#         "kv_role": "kv_consumer"
-#     }' \
-vllm serve --model="/home/fq9hpsac/fq9hpsacuser03/deepseek-v2-lite" \
-    --data-parallel-size 2 \
-    --enforce-eager \
-    -tp 1 \
-    --port 8022 \
-    --enable-expert-parallel > normal.log 2>&1 &
+#     --port 8022 \
+#     --enable-expert-parallel > normal.log 2>&1 &
 
 # --capture-range=cudaProfilerApi \
 #     --capture-range-end repeat \
