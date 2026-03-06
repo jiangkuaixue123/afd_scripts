5dfe218a8fb550f6b63093a4a14fe64970086dc5
jiangkuaixue123 <jiangxiaozhou111@163.com>
2026-03-06 14:56:03 +0800

    add
    Signed-off-by: jiangkuaixue123 <jiangxiaozhou111@163.com>

--- run_temp_attn.sh
+++ run_temp_attn.sh
@@ -8,5 +8,9 @@ export RANK=1
 export MASTER_ADDR=10.248.12.142
 export MASTER_PORT=29500
 export NCCL_IB_DISABLE=$2
-
+export NCCL_DEBUG=TRACE
+export NCCL_DEBUG_SUBSYS=ALL
+# 使用 %h (hostname) 和 %p (pid) 来区分不同进程的日志
+# NCCL 原生支持 %h 和 %p，但不支持 %r (rank) 除非在特定环境下
+export NCCL_DEBUG_FILE=nccl_rank_attn_%h_%p.log
 python pynccl_demo.py > temp_attn.log 2>&1 &
