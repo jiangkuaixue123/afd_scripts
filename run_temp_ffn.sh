22613c81170af15bae5aa45863180b2a813706a9
jiangkuaixue123 <jiangxiaozhou111@163.com>
2026-03-09 10:43:56 +0800

    add
    Signed-off-by: jiangkuaixue123 <jiangxiaozhou111@163.com>

--- run_temp_ffn.sh
+++ run_temp_ffn.sh
@@ -7,11 +7,11 @@ export WORLD_SIZE=2
 export RANK=0
 export MASTER_ADDR=10.248.12.142
 export MASTER_PORT=29500
-export NCCL_NET_PLUGIN=none
-export NCCL_IB_DISABLE=1
-export NCCL_DEBUG=TRACE
-export NCCL_DEBUG_SUBSYS=ALL
-# 使用 %h (hostname) 和 %p (pid) 来区分不同进程的日志
-# NCCL 原生支持 %h 和 %p，但不支持 %r (rank) 除非在特定环境下
-export NCCL_DEBUG_FILE=nccl_rank_ffn_%h_%p.log
-NCCL_IB_DISABLE=1 python pynccl_demo.py > temp_ffn.log 2>&1 &
+# export NCCL_NET_PLUGIN=none
+# export NCCL_IB_DISABLE=1
+# export NCCL_DEBUG=TRACE
+# export NCCL_DEBUG_SUBSYS=ALL
+# # 使用 %h (hostname) 和 %p (pid) 来区分不同进程的日志
+# # NCCL 原生支持 %h 和 %p，但不支持 %r (rank) 除非在特定环境下
+# export NCCL_DEBUG_FILE=nccl_rank_ffn_%h_%p.log
+python pynccl_p2p_cudagraph.py > temp_ffn.log 2>&1 &
