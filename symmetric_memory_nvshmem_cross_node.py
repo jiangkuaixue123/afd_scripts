78fa6c0f6e9fa9b4604a5da729b7191ea7dc42e2
jiangkuaixue123 <jiangxiaozhou111@163.com>
2026-03-10 17:18:37 +0800

    add
    Signed-off-by: jiangkuaixue123 <jiangxiaozhou111@163.com>

--- symmetric_memory_nvshmem_cross_node.py
+++ symmetric_memory_nvshmem_cross_node.py
@@ -103,7 +103,8 @@ def nvshmem_put_signal_kernel(
     nvshmem.quiet()
     # Use signal_op to set the signal value on remote peer
     # NVSHMEM_SIGNAL_SET = 0 (set signal to the given value)
-    nvshmem.signal_op(signal_ptr, signal_val, 0, peer)
+    # signal_op expects int64 address
+    nvshmem.signal_op(signal_ptr.to(tl.int64), signal_val, 0, peer)
 
 
 @requires_nvshmem
