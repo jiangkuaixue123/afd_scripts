3ea624a1623ef563b06725d56809773ab7c7162a
jiangkuaixue123 <jiangxiaozhou111@163.com>
2026-03-10 16:08:30 +0800

    add
    Signed-off-by: jiangkuaixue123 <jiangxiaozhou111@163.com>

--- run_attn.sh
+++ run_attn.sh
@@ -22,7 +22,7 @@ export NCCL_GRAPH_MIXING_SUPPORT=1
 # export NCCL_P2P_NET_CHUNKSIZE=262144
 export NCCL_P2P_NET_CHUNKSIZE=524288
 export NCCL_NET_PLUGIN=none
-# export NCCL_IB_DISABLE=1
+export NCCL_IB_DISABLE=1
 export NCCL_SET_THREAD_NAME=1
 
 vllm serve "/home/fq9hpsac/fq9hpsacuser03/deepseek-v2-lite" \
